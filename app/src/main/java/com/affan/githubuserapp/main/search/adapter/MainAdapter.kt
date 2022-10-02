f1b6fbf5ba5e219723671d835c51a6c0ec9fc220
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-02 17:08:51 +0700

    Make Adapter for search list

--- app/src/main/java/com/affan/githubuserapp/main/search/adapter/MainAdapter.kt
+++ app/src/main/java/com/affan/githubuserapp/main/search/adapter/MainAdapter.kt
@@ -1,4 +1,32 @@
 package com.affan.githubuserapp.main.search.adapter
 
-class MainAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.affan.githubuserapp.databinding.ContainerListUserBinding
+
+class MainAdapter : RecyclerView.Adapter<MainAdapter.MainViewModel>() {
+
+//    private val itemList = listOf<>()
+
+    inner class MainViewModel (private val binding: ContainerListUserBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewModel {
+        return MainViewModel(ContainerListUserBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MainViewModel, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
