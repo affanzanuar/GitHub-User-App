dd7ebfdd3c073bd54b8c003046e34ba36c767a17
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-03 22:21:10 +0700

    Make fun getSearchUser in ApiService.kt

--- app/src/main/java/com/affan/githubuserapp/data/remote/ApiService.kt
+++ app/src/main/java/com/affan/githubuserapp/data/remote/ApiService.kt
@@ -1,8 +1,16 @@
 package com.affan.githubuserapp.data.remote
 
+import com.affan.githubuserapp.data.model.user.User
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
 
     @GET
-
+    fun getSearchUsers (
+        @Query("q") userName : String,
+        @Query("per_page") perPage : Int,
+        @Query("page") page : Int
+    ) : User
 
 }
