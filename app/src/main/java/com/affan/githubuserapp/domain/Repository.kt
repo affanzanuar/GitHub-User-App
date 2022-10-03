69ddae30664fedd82eb8412ff58f300e700ae157
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-03 22:23:20 +0700

    Make Repository.kt

--- app/src/main/java/com/affan/githubuserapp/domain/Repository.kt
+++ app/src/main/java/com/affan/githubuserapp/domain/Repository.kt
@@ -1,7 +1,13 @@
 package com.affan.githubuserapp.domain
 
+import com.affan.githubuserapp.data.model.user.User
+
 interface Repository {
 
-
+    fun getSearchUsers (
+        userName : String,
+        perPage : Int,
+        page : Int
+    ) : User
 
 }
