5e364c0e822f1f9cdbdaf77830d634a4205f80cc
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-03 22:54:18 +0700

    Make ViewModelFactory.kt for injection

---
+++ app/src/main/java/com/affan/githubuserapp/main/search/viewmodel/SearchViewModel.kt
@@ -0,0 +1,9 @@
+package com.affan.githubuserapp.main.search.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.affan.githubuserapp.domain.Repository
+
+class SearchViewModel (
+    private val repository: Repository
+        ) : ViewModel() {
+}
