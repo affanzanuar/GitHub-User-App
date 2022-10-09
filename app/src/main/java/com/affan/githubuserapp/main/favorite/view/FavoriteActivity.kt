fc498ba0cab08ed7ee40762af23d9c87a0760748
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-09 23:11:03 +0700

    Set Up adapter in FavoriteActivity.kt

--- app/src/main/java/com/affan/githubuserapp/main/favorite/view/FavoriteActivity.kt
+++ app/src/main/java/com/affan/githubuserapp/main/favorite/view/FavoriteActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.affan.githubuserapp.databinding.ActivityFavoriteBinding
 import com.affan.githubuserapp.di.ViewModelFactory
+import com.affan.githubuserapp.main.favorite.adapter.FavoriteAdapter
 import com.affan.githubuserapp.main.favorite.viewmodel.FavoriteViewModel
 
 class FavoriteActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityFavoriteBinding
+
+    private lateinit var favoriteAdapter: FavoriteAdapter
 
     private lateinit var viewModel: FavoriteViewModel
 
@@ -25,13 +29,23 @@ class FavoriteActivity : AppCompatActivity() {
             ViewModelFactory.getInstance(this)
         )[FavoriteViewModel::class.java]
 
+        setupAdapter()
+        getObserve()
+        viewModel.getAllWatchList()
+
         binding.ivBack.setOnClickListener {
             finish()
         }
 
     }
 
-    fun getObserve () {
+    private fun setupAdapter () {
+        favoriteAdapter = FavoriteAdapter()
+        binding.rvWatchList.adapter = favoriteAdapter
+        binding.rvWatchList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun getObserve () {
         viewModel.isLoading.observe(this) { isLoading ->
             if (isLoading){
                 binding.rvWatchList.visibility = View.GONE
@@ -42,10 +56,14 @@ class FavoriteActivity : AppCompatActivity() {
             }
         }
 
+        viewModel.watchList.observe(this) { data ->
+            favoriteAdapter.clearData()
+            favoriteAdapter.setData(data)
+        }
+
         viewModel.error.observe(this) { error ->
             Log.e("what Favorite Activity", error)
         }
-
     }
 
 }
