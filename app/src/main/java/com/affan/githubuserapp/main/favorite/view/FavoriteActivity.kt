daa19d701802c9b8500166c638d66cd45a6d6138
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-09 23:05:49 +0700

    Add fun getObserve in FavoriteActivity.kt and add Loading

--- app/src/main/java/com/affan/githubuserapp/main/favorite/view/FavoriteActivity.kt
+++ app/src/main/java/com/affan/githubuserapp/main/favorite/view/FavoriteActivity.kt
@@ -2,7 +2,11 @@ package com.affan.githubuserapp.main.favorite.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.affan.githubuserapp.databinding.ActivityFavoriteBinding
+import com.affan.githubuserapp.di.ViewModelFactory
 import com.affan.githubuserapp.main.favorite.viewmodel.FavoriteViewModel
 
 class FavoriteActivity : AppCompatActivity() {
@@ -16,6 +20,11 @@ class FavoriteActivity : AppCompatActivity() {
         binding = ActivityFavoriteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelFactory.getInstance(this)
+        )[FavoriteViewModel::class.java]
+
         binding.ivBack.setOnClickListener {
             finish()
         }
@@ -23,6 +32,20 @@ class FavoriteActivity : AppCompatActivity() {
     }
 
     fun getObserve () {
+        viewModel.isLoading.observe(this) { isLoading ->
+            if (isLoading){
+                binding.rvWatchList.visibility = View.GONE
+                binding.skLoadingFavorite.visibility = View.VISIBLE
+            } else {
+                binding.rvWatchList.visibility = View.VISIBLE
+                binding.skLoadingFavorite.visibility = View.GONE
+            }
+        }
+
+        viewModel.error.observe(this) { error ->
+            Log.e("what Favorite Activity", error)
+        }
+
     }
 
 }
